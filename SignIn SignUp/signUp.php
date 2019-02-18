cb30464c5e575c89e390fa3dcb0e70d052972bfa
c20808ar <abdulrahman.rafea@student.manchester.ac.uk>
2019-02-18 22:51:05 +0000

    fixed error message in sign up page

--- SignIn SignUp/signUp.php
+++ SignIn SignUp/signUp.php
@@ -122,7 +122,8 @@ if ($_SERVER["REQUEST_METHOD"] == "POST")
 ?>
 		<div class="fixed-top">
 	<div class="alert alert-danger alert-dismissible fade show" role="alert">
-  <strong>Error:</strong>please fill all fields correctly.
+  <strong>Error:</strong> Please fill in all the fields correctly.
+                          Already a member? <a href="signIn.php" class="alert-link"> Sign In Here!</a>
   <button type="button" class="close" data-dismiss="alert" aria-label="Close">
     <span aria-hidden="true">&times;</span>
   </button>
