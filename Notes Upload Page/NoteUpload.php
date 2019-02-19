588bc731b8bf9767a68bc6377ef78612f4c966d2
Bozhidar Nedelchev <bozhidar.nedelchev@student.manchester.ac.uk>
2019-02-19 14:15:32 +0000

    fix error v14

--- Notes Upload Page/NoteUpload.php
+++ Notes Upload Page/NoteUpload.php
@@ -16,7 +16,7 @@
       $name = $_FILES['requiredFile']['name'];
       $type = $_FILES['requiredFile']['type'];
       $data = file_get_contents($_FILES['requiredFile']['tmp_name']);
-      $stmt = $conn->prepare("INSERT INTO `Notes` VALUES ("",?, 1, 1, ?, ?, "")");
+      $stmt = $conn->prepare("INSERT INTO `Notes` VALUES ('', ?, ?, ?);
       $stmt->bindParam(1, $name);
       $stmt->bindParam(2, $type);
       $stmt->bindParam(3, $data);
