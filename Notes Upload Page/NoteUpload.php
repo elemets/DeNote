47d0e87f7fa6eacdee5f438b3a69c7c32a28e2b3
[salim alwahaibi] <[salim.al-wahaibi@student.manchetser.ac.uk]>
2019-02-19 16:42:58 +0000

    upload fix

--- Notes Upload Page/NoteUpload.php
+++ Notes Upload Page/NoteUpload.php
@@ -14,11 +14,12 @@ session_start();
     require_once('config.inc.php');
     // Connect to the database
      $conn = new PDO("mysql:host=$database_host;dbname=$database_name", $database_user, $database_pass);
+     $conn2 = new mysqli($database_host, $database_user, $database_pass, "2018_comp10120_z3");
 
      $conn->setAttribute(PDO::ATTR_ERRMODE, PDO::ERRMODE_EXCEPTION);
 
     if(isset($_POST['btn'])){
-      $userID = mysql_fetch_object($conn->query("SELECT UserID FROM Users WHERE Username ='$_SESSION[username]'"));
+      $userID = mysql_fetch_object($conn2->query("SELECT UserID FROM Users WHERE Username ='$_SESSION[username]'"));
       $name = $_FILES['requiredFile']['name'];
       $type = $_FILES['requiredFile']['type'];
       $data = file_get_contents($_FILES['requiredFile']['tmp_name']);
