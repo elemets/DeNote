a8c8a929d0abd29351f31660fc64d79291654c47
Bozhidar Nedelchev <bozhidar.nedelchev@student.manchester.ac.uk>
2019-02-19 18:35:26 +0000

    showing notes 8

--- Profile Page/view.php
+++ Profile Page/view.php
@@ -2,12 +2,9 @@
 session_start();
 require_once('config.inc.php');
 $conn = new PDO("mysql:host=$database_host;dbname=$database_name", $database_user, $database_pass);
-$conn2 = new mysqli($database_host, $database_user, $database_pass, "2018_comp10120_z3");
-echo $_SESSION['username'];
-$userID = $conn2->query("SELECT UserID FROM Users WHERE Username ='$_SESSION[username]'")->fetch_object()->UserID;
-echo $userID;
-$stat = $conn->prepare("SELECT * FROM Notes WHERE UserID = ?");
-$stat->bindParam(1, $userID);
+$id = isset($_GET['id'])? $_GET['id'] : "";
+$stat = $conn->prepare("SELECT * FROM Notes WHERE NoteID = ?");
+$stat->bindParam(1, $id);
 $stat->execute();
 
 header('Content-Type:'.$row['dataType']);
