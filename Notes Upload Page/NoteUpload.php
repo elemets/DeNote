9a3c1b5b40e178e2f5f282feacd8313da8505a95
Bozhidar Nedelchev <bozhidar.nedelchev@student.manchester.ac.uk>
2019-02-19 14:04:41 +0000

    fix error v10

--- Notes Upload Page/NoteUpload.php
+++ Notes Upload Page/NoteUpload.php
@@ -11,15 +11,15 @@
     require_once('config.inc.php');
     // Connect to the database
      $conn = new PDO("mysql:host=$database_host;dbname=$database_name", $database_user, $database_pass);
-    
+
     if(isset($_POST['btn'])){
       $name = $_FILES['requiredFile']['name'];
       $type = $_FILES['requiredFile']['type'];
       $data = file_get_contents($_FILES['requiredFile']['tmp_name']);
       $stmt = $conn->prepare("INSERT INTO `Notes`(`FileName`, `dataType`, `Data`) VALUES (?,?,?)");
-      $stmt->bind_param(1, $name);
-      $stmt->bind_param(2, $type);
-      $stmt->bind_param(3, $data);
+      $stmt->bindParam(1, $name);
+      $stmt->bindParam(2, $type);
+      $stmt->bindParam(3, $data);
       $stmt->execute();
     }
 
