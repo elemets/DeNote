cddf1f68f3ecc722d8a35c3678a1a077fac6f41f
Bozhidar Nedelchev <bozhidar.nedelchev@student.manchester.ac.uk>
2019-02-19 18:14:04 +0000

    showing notes

---
+++ Profile Page/profile-showingNotes.php
@@ -0,0 +1,15 @@
+<?php
+session_start();
+require_once('config.inc.php');
+$conn = new PDO("mysql:host=$database_host;dbname=$database_name", $database_user, $database_pass);
+$conn2 = new mysqli($database_host, $database_user, $database_pass, "2018_comp10120_z3");
+echo $_SESSION[username];
+$userID = $conn2->query("SELECT UserID FROM Users WHERE Username ='$_SESSION[username]'")->fetch_object()->UserID;
+echo $userID;
+$stat = $conn->prepare("SELECT * FROM Notes WHERE UserID = ?");
+$stat->bindParam(1, $userID)
+$stat->execute();
+
+header('Content-Type:'.$row['dataType']);
+echo $row['Data'];
+ ?>
---
+++ Profile Page/profile-test-viewing.php
@@ -0,0 +1,19 @@
+<!DOCTYPE html>
+<html>
+  <head>
+    <meta charset="utf-8">
+    <title>Profile Test</title>
+  </head>
+  <body>
+    <?php
+    $conn = new PDO("mysql:host=$database_host;dbname=$database_name", $database_user, $database_pass);
+    $stat = $conn->prepare("SELECT * FROM Notes");
+    $stat->execute();
+
+    while($row = $stat->fetch()){
+      echo "<li><a target='_blank' href='profile-showingNotes.php?id=".$row['NoteID']."'>".$row['FileName']."</a><br/>
+      <embed src='Data:'.$row['dataType'].";base64,".base64_encode($row['Data'])."' width='200'/></li>";
+    }
+     ?>
+  </body>
+</html>
