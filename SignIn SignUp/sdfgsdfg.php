82510f28fb721c9ec708c75d247e39b8e11839a7
Abdulrahman <abdulrahman.rafea@student.manchester.ac.uk>
2019-02-20 10:48:34 +0000

    Styling sign in page

--- SignIn SignUp/sdfgsdfg.php
+++ SignIn SignUp/sdfgsdfg.php
@@ -69,18 +69,18 @@ session_start();
             <div class="form-body">
               <form role="form" method="post" action="<?php echo $_SERVER['PHP_SELF'];?>">
 
-    		  <div class="form-group">
+    		  <div class="form-group username">
                 <label for="username"> Username</label>
                 <input type="textbox" class="form-control form-element" name="username" placeholder="Username">
               </div>
 
-              <div class="form-group">
+              <div class="form-group password">
                 <label for="password"> Password</label>
     		    <input type="password" class="form-control form-element" name="password" placeholder="Password">
               </div>
 
               <br>
-	      <input type="submit" class="btn btn-default btn-lg submit-btn btn-block submit-font bottom-buffer" value="Sign In">
+	      <input type="submit" class="btn btn-default btn-lg submit-btn btn-block submit-font bottom-buffer" value="Submit">
 
               </form>
             </div>
