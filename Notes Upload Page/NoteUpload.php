a87c55e384ecd54c55935b3f8676178987984dd5
[salim alwahaibi] <[salim.al-wahaibi@student.manchetser.ac.uk]>
2019-02-21 17:28:16 +0000

    change upload

--- Notes Upload Page/NoteUpload.php
+++ Notes Upload Page/NoteUpload.php
@@ -25,7 +25,7 @@ session_start();
       $data = file_get_contents($_FILES['requiredFile']['tmp_name']);
       $sectionName = $_POST["sectionName"];
       $parentID = $_POST["courses"];
-      $stmt = $conn->prepare("INSERT INTO Notes (`FileName`,`dataType`,`Data`, `SectionName`, `UserID`, `ParentID`) VALUES (?, ?, ?,?, ?,?)");
+      $stmt = $conn->prepare("INSERT INTO Notes (`FileName`,`dataType`,`Data`, `SectionNumber`, `UserID`, `UnitID`) VALUES (?, ?, ?,?, ?,?)");
       $stmt->bindParam(1, $name);
       $stmt->bindParam(2, $type);
       $stmt->bindParam(3, $data);
