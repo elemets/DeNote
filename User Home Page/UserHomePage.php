1a9a0f0b9eb4231cd915d55fc068241076512ec7
[salim alwahaibi] <[salim.al-wahaibi@student.manchetser.ac.uk]>
2019-02-23 09:17:06 +0000

    working on making notes appear in the user homePage

--- User Home Page/UserHomePage.php
+++ User Home Page/UserHomePage.php
@@ -13,8 +13,13 @@ require_once("../Header - Footer/header.html");
 <?php
 $_SESSION["username"] = $_SESSION["username"]
 ?>
-
-
+<?php echo "welcome " + $_SESSION["username"];
+	<div class="jumbotron Container-fluid">
+          <img src="Icons/Notes_Icons/PurpleNoHover.png" class="hoverable">
+          <div class="centered">COMP16212</div>
+          <img src="Icons/Notes_Icons/PurpleNoHover.png" class="hoverable">
+          <div class="">COMP1020</div>
+        </div>
 </body>
 <footer>
 <?php
