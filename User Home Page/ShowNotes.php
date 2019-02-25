ab1384905e714a6bd974cd17e9481128445a1e0c
[salim alwahaibi] <[salim.al-wahaibi@student.manchetser.ac.uk]>
2019-02-25 21:57:30 +0000

    trying to implement the user home page

--- User Home Page/ShowNotes.php
+++ User Home Page/ShowNotes.php
@@ -3,8 +3,9 @@ session_start();
 require_once('config.inc.php');
 $conn = new PDO("mysql:host=$database_host;dbname=$database_name", $database_user, $database_pass);
 $id = isset($_GET['id'])? $_GET['id'] : "";
-$stat = $conn->prepare("SELECT * FROM Notes WHERE UnitID = ?");
+$stat = $conn->prepare("SELECT * FROM Notes WHERE UnitID = ? AND UnitYear = ?");
 $stat->bindParam(1, $id);
+$stat->bindParam(2, $_SESSION[username]);
 $stat->execute();
 
 while($row = $stat->fetch()){
