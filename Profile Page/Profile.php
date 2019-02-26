bd155bf32550efeb4fca1dbf3f7814a65c62e75b
Arthur <arthur.funnell@student.manchester.ac.uk>
2019-02-26 13:28:43 +0000

    Nav Bar fix

--- Profile Page/Profile.php
+++ Profile Page/Profile.php
@@ -7,16 +7,6 @@ require_once("../Header - Footer/header.html");
 ?>
 <!-- Bootstrap CSS -->
 
-  <meta charset="utf-8">
-  <meta name="viewport" content="width=device-width, initial-scale=1">
-  <link rel="stylesheet" href="https://maxcdn.bootstrapcdn.com/bootstrap/3.4.0/css/bootstrap.min.css">
-
-  <link rel="stylesheet" href="ProfilePageCss.css">
-  <link href="https://fonts.googleapis.com/css?family=Montserrat" rel="stylesheet" type="text/css">
-  <link href="https://fonts.googleapis.com/css?family=Lato" rel="stylesheet" type="text/css">
-  <link rel="stylesheet" href="mdb.css">
-  <script src="https://ajax.googleapis.com/ajax/libs/jquery/3.3.1/jquery.min.js"></script>
-  <script src="https://maxcdn.bootstrapcdn.com/bootstrap/3.4.0/js/bootstrap.min.js"></script>
 
 </head>
 <body> <!-- Nav Bar -->
