ae8877b6f266d030b8ab401a9326e488a28c64a9
c20808ar <abdulrahman.rafea@student.manchester.ac.uk>
2019-02-27 20:03:23 +0000

    Styling upload 1

--- Notes Upload Page/NoteUpload.php
+++ Notes Upload Page/NoteUpload.php
@@ -3,12 +3,14 @@ session_start();
 ?>
 <!DOCTYPE html>
 <html>
-<meta name="viewport" content="width=device-width, initial-scale=1">
-<link rel="stylesheet" href="https://stackpath.bootstrapcdn.com/bootstrap/4.3.0/css/bootstrap.min.css">
-<meta name="viewport" content="width=device-width, initial-scale=1">
-<link rel="stylesheet" href="https://www.w3schools.com/w3css/4/w3.css">
-<script src="https://ajax.googleapis.com/ajax/libs/jquery/3.3.1/jquery.min.js"></script>
+<head>
+  <meta name="viewport" content="width=device-width, initial-scale=1">
+  <link rel="stylesheet" href="https://stackpath.bootstrapcdn.com/bootstrap/4.3.0/css/bootstrap.min.css">
+  <meta name="viewport" content="width=device-width, initial-scale=1">
+  <link rel="stylesheet" href="https://www.w3schools.com/w3css/4/w3.css">
+  <script src="https://ajax.googleapis.com/ajax/libs/jquery/3.3.1/jquery.min.js"></script>
 <script src="https://maxcdn.bootstrapcdn.com/bootstrap/3.4.0/js/bootstrap.min.js"></script>
+</head>
 <style>
 
     body {
@@ -56,9 +58,6 @@ session_start();
 
     <title>Upload Page</title>
 
-<?php
-include "../Header - Footer/header.php";
-?>
 <body>
 
 <?php
@@ -157,8 +156,4 @@ if(isset($_POST['btn']))
      ?>
 
 </body>
-
-<?php
-include "../Header - Footer/footer.html";
-?>
 </html>
