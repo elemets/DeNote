2cdd3bec45476106fae9e34624dc534698776bb3
Bozhidar Nedelchev <bozhidar.nedelchev@student.manchester.ac.uk>
2019-02-27 15:07:51 +0000

    search

--- User Home Page/search.php
+++ User Home Page/search.php
@@ -8,7 +8,14 @@
     <?php
     require_once('config.inc.php');
     $conn = new mysqli($database_host, $database_user, $database_pass, "2018_comp10120_z3");
-    echo $_POST['searchWord'];
+    $searchWord = $_POST['searchWord'];
+    $stat = $conn->prepare("SELECT * FROM Notes WHERE * = ?");
+    $stat->bindParam(1, $searchWord);
+    $stat->execute();
+
+    while($row = $stat->fetch()){
+      echo "<li><a target='_blank' href='view.php?id=".$row['NoteID']."'>".$row['FileName']."</a></li>";
+    }
     ?>
   </body>
 </html>
