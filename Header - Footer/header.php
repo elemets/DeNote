3d0b60a744d56b21d031340dcfc8745e4e238a22
c20808ar <abdulrahman.rafea@student.manchester.ac.uk>
2019-02-27 20:46:40 +0000

    Styling upload 1

--- Header - Footer/header.php
+++ Header - Footer/header.php
@@ -5,7 +5,6 @@
     <meta charset="utf-8">
     <meta name="viewport" content="width=device-width, initial-scale=1">
     <link rel="stylesheet" href="https://maxcdn.bootstrapcdn.com/bootstrap/3.4.0/css/bootstrap.min.css">
-    <link rel="stylesheet" href="../universalStyleSheet.css">
     <link href="https://fonts.googleapis.com/css?family=Montserrat" rel="stylesheet" type="text/css">
     <link href="https://fonts.googleapis.com/css?family=Lato" rel="stylesheet" type="text/css">
     <script src="https://ajax.googleapis.com/ajax/libs/jquery/3.3.1/jquery.min.js"></script>
