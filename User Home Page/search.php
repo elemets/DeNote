83bd95658fdd12cb9ef7ee15a84bda253050562a
Bozhidar Nedelchev <bozhidar.nedelchev@student.manchester.ac.uk>
2019-02-27 15:37:49 +0000

    search

--- User Home Page/search.php
+++ User Home Page/search.php
@@ -1,6 +1,3 @@
-<?php
-session_start();
-?>
 <!DOCTYPE html>
 <html>
   <head>
@@ -10,24 +7,19 @@ session_start();
   <body>
     <?php
     require_once('config.inc.php');
-    //$conn = new PDO("mysql:host=$database_host;dbname=$database_name", $database_user, $database_pass);
-    $conn = new mysqli($database_host, $database_user, $database_pass, "2018_comp10120_z3");
+    $conn = new PDO("mysql:host=$database_host;dbname=$database_name", $database_user, $database_pass);
+    // $conn = new mysqli($database_host, $database_user, $database_pass, "2018_comp10120_z3");
     $searchWord = $_POST['searchWord'];
-    //$userID = $conn2->query("SELECT UserID FROM Users WHERE Username ='$_SESSION[username]")->fetch_object()->UserID;//userID query
     $stat = $conn->prepare("SELECT * FROM Notes");
     $stat->bindParam(1, $searchWord);
     $stat->execute();
 
     while($row = $stat->fetch())
     {
-      if ($row['FileName'] == $searchWord || $row['TitleNote'] == $searchWord || $row['UnitID'] == $searchWord )
+      if ($row['FileName'] == $searchWord || $row['TitleNote'] == $searchWord || $row['UnitID'] == $searchWord)
       {
-        echo "<li><a target='_blank' href='view.php?id=".$row['NoteID']."'>".$row['FileName']."</a></li>";
+              echo "<li><a target='_blank' href='view.php?id=".$row['NoteID']."'>".$row['FileName']."</a></li>";
       }
-      //if ($searchWord == $conn2->query("SELECT Username FROM Users WHERE UserID = $row['UnitID']")->fetch_object()->Username)
-      //{
-      //    echo "<li><a target='_blank' href='view.php?id=".$row['NoteID']."'>".$row['FileName']."</a></li>";
-      //}
     }
     ?>
   </body>
