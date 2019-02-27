2f2d6b0721660c0ae994c438fb1a538fa12ca727
c20808ar <abdulrahman.rafea@student.manchester.ac.uk>
2019-02-27 20:16:41 +0000

    Styling upload 1

--- Notes Upload Page/NoteUpload.php
+++ Notes Upload Page/NoteUpload.php
@@ -1,15 +1,49 @@
-<?php
-session_start();
-?>
 <!DOCTYPE html>
-<html>
+<html lang="en">
+    <!-- Theme Made By www.w3schools.com -->
+    <title>DeNOTE</title>
+    <meta charset="utf-8">
+    <meta name="viewport" content="width=device-width, initial-scale=1">
+    <link rel="stylesheet" href="https://maxcdn.bootstrapcdn.com/bootstrap/3.4.0/css/bootstrap.min.css">
+    <link rel="stylesheet" href="../universalStyleSheet.css">
+    <link href="https://fonts.googleapis.com/css?family=Montserrat" rel="stylesheet" type="text/css">
+    <link href="https://fonts.googleapis.com/css?family=Lato" rel="stylesheet" type="text/css">
+    <script src="https://ajax.googleapis.com/ajax/libs/jquery/3.3.1/jquery.min.js"></script>
+    <script src="https://maxcdn.bootstrapcdn.com/bootstrap/3.4.0/js/bootstrap.min.js"></script>
 
-
-    <title>Upload Page</title>
-
-<?php
-include "../Header - Footer/header.php";
-?>
+  <head id="myPage" data-spy="scroll" data-target=".navbar" data-offset="60">
+    <nav class="navbar navbar-default navbar-fixed-top">
+      <div class="container">
+        <div class="navbar-header">
+          <button type="button" class="navbar-toggle" data-toggle="collapse" data-target="#myNavbar">
+          <span class="icon-bar"></span>
+          <span class="icon-bar"></span>
+          <span class="icon-bar"></span>
+          </button>
+          <a class="navbar-brand" href="../User Home Page/UserHomePage.php"><img src="../Header - Footer/logo_white.png"  height="23.5"> </a>
+        </div>
+        <div class="collapse navbar-collapse" id="myNavbar">
+          <ul class="nav navbar-nav navbar-center">
+            <li><a href="../Feed Page/Feed.php"><span class="glyphicon glyphicon-list"></span> FEED</a></li>
+            <li><a href="../Notes Upload Page/NoteUpload.php"><span class="glyphicon glyphicon-upload"></span> UPLOAD</a></li>
+          </ul>
+          <form method="post" class="navbar-form navbar-left" action="search.php">
+            <div class="form-group">
+              <input type="text" class="form-control" placeholder="Looking for Something?" name="searchWord">
+            </div>
+            <button type="submit" class="btn"><a>
+            <span class="glyphicon glyphicon-search"></span>
+            </a></button>
+          </form>
+          <ul class="nav navbar-nav navbar-right">
+            <li><a href="../Profile Page/Profile.php"><span class="glyphicon glyphicon-user"></span>PROFILE</a></li>
+            <li><a href="../index.html"><span class="glyphicon glyphicon-log-out"></span>LOGOUT</a></li>
+          </ul>
+        </div>
+      </div>
+    </nav>
+  </head>
+  
 <style>
     body {
         background-color: white !important;
