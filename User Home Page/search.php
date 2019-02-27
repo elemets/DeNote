0afd54b11b451ef075046a47370cfbc442a63d4c
Bozhidar Nedelchev <bozhidar.nedelchev@student.manchester.ac.uk>
2019-02-27 14:55:36 +0000

    search

--- User Home Page/search.php
+++ User Home Page/search.php
@@ -8,7 +8,7 @@
     <?php
     require_once('config.inc.php');
     $conn = new mysqli($database_host, $database_user, $database_pass, "2018_comp10120_z3");
-    echo $_GET['searchWord'];
+    echo $_POST['searchWord'];
     ?>
   </body>
 </html>
