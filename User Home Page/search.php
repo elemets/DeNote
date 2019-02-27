8c992e035f7b7bb7f3801cf5ae2d021777a767f0
Bozhidar Nedelchev <bozhidar.nedelchev@student.manchester.ac.uk>
2019-02-27 15:16:48 +0000

    search

--- User Home Page/search.php
+++ User Home Page/search.php
@@ -10,12 +10,16 @@
     $conn = new PDO("mysql:host=$database_host;dbname=$database_name", $database_user, $database_pass);
     // $conn = new mysqli($database_host, $database_user, $database_pass, "2018_comp10120_z3");
     $searchWord = $_POST['searchWord'];
-    $stat = $conn->prepare("SELECT * FROM Notes WHERE * = ?");
+    $stat = $conn->prepare("SELECT * FROM Notes");
     $stat->bindParam(1, $searchWord);
     $stat->execute();
 
-    while($row = $stat->fetch()){
-      echo "<li><a target='_blank' href='view.php?id=".$row['NoteID']."'>".$row['FileName']."</a></li>";
+    while($row = $stat->fetch())
+    {
+      if ($row['FileName'] == $searchWord || $row['titleNote'] == $searchWord || $row['UnitID'] == $searchWord)
+      {
+              echo "<li><a target='_blank' href='view.php?id=".$row['NoteID']."'>".$row['FileName']."</a></li>";
+      }
     }
     ?>
   </body>
