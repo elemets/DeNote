c826a75a95ca807f5756b022e125f5efb3baea51
c20808ar <abdulrahman.rafea@student.manchester.ac.uk>
2019-02-27 19:08:31 +0000

    Styling upload 1

--- Notes Upload Page/NoteUpload.php
+++ Notes Upload Page/NoteUpload.php
@@ -43,8 +43,6 @@ session_start();
         padding-top: 50px;
         min-height: 100%;  /* Fallback for browsers do NOT support vh unit */
         min-height: 100vh; /* These two lines are counted as one :-)       */
-        width: auto;
-        display: flex;
         align-items: center;
     }
 </style>
