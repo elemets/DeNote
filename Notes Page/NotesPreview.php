0ee776919172eb0af16d5cc7fe83cb4838e3d5d0
[salim alwahaibi] <[salim.al-wahaibi@student.manchetser.ac.uk]>
2019-02-28 20:59:06 +0000

    making notes appear

--- Notes Page/NotesPreview.php
+++ Notes Page/NotesPreview.php
@@ -10,7 +10,7 @@
 ?>
 <iframe src="view.php?id=".$_GET['id']  width="960" height="900">
  </iframe>
-<?php echo $row['NoteID'] ?>
+<?php echo $_GET['id'] ?>
 <!-- begin wwww.htmlcommentbox.com -->
  <div id="HCB_comment_box" style="width:80%;margin-left:10%"><a href="http://www.htmlcommentbox.com">Widget</a> is loading comments...</div>
  <link rel="stylesheet" type="text/css" href="//www.htmlcommentbox.com/static/skins/bootstrap/twitter-bootstrap.css?v=0" />
