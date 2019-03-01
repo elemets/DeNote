63d988842465546f37fe84c1a18fe06456061bf9
Martin <martin.cuestaallende@student.manchester.ac.uk>
2019-03-01 17:11:34 +0000

    Removed comment containing code of old navbar

--- Profile Page/Profile.php
+++ Profile Page/Profile.php
@@ -7,35 +7,9 @@ require_once("../Header - Footer/header.php");
 ?>
 <!-- Bootstrap CSS -->
 
-
 </head>
-<body id="top"> <!-- Nav Bar -->
+<body id="top">
 
-
-
-<!--  <nav class="navbar navbar-default navbar-fixed-top">
-    <div class="container">
-      <div class="navbar-header">
-        <button type="button" class="navbar-toggle" data-toggle="collapse" data-target="#myNavbar">
-          <span class="icon-bar"></span>
-          <span class="icon-bar"></span>
-          <span class="icon-bar"></span>
-        </button>
-        <a class="navbar-brand" href="#WelcomePage"><img src="Icons/logo_white.png"  height="23.5"> </a>
-      </div>
-      <div class="collapse navbar-collapse" id="myNavbar">
-        <ul class="nav navbar-nav navbar-center">
-          <li><a href="#My_notes">MY NOTES</a></li>
-          <li><a href="#Following">FOLLOWING</a></li>
-          <li><a href="#Followers">MY FOLLOWERS</a></li>
-        </ul>
-        <ul class="nav navbar-nav navbar-right">
-          <li><a href="SignIn SignUp/signUp.php"><span class="glyphicon glyphicon-user"></span>SIGN UP</a></li>
-          <li><a href="SignIn SignUp/signIn.php"><span class="glyphicon glyphicon-log-in"></span>LOGIN</a></li>
-        </ul>
-      </div>
-    </div>
-  </nav> -->
 <!-- Title and profile icon -->
 <div id="My_Profile" class="text-center">
   <h1 style="font-size:60px;padding-top: 55px;">My Profile</h1>
