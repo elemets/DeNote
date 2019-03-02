f16ac23b5780dafaf93b4420607a911df51c8fe5
Arthur Funnell <arthur.funnell@student.manchester.ac.uk>
2019-03-02 15:12:00 +0000

    Adding active

--- Header - Footer/header.php
+++ Header - Footer/header.php
@@ -25,6 +25,16 @@
             }
         });
     });
+
+    $(function(){
+    var current = location.pathname;
+    $('#nav li a').each(function(){
+        var $this = $(this);
+        // if the current path is like this link, make it active
+        if($this.attr('href').indexOf(current) !== -1){
+            $this.addClass('active');
+        }
+    })
     </script>
 
     <nav class="navbar navbar-default navbar-fixed-top">
