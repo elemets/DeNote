359120b77c93d0e28dd40a769efa89a27fd34b36
Arthur Funnell <arthur.funnell@student.manchester.ac.uk>
2019-03-02 14:30:22 +0000

    Adding active

--- Header - Footer/header.php
+++ Header - Footer/header.php
@@ -18,7 +18,7 @@
     <script type="text/javascript">
     $(function(){
         var current = location.pathname;
-        $('#navbar-nav li a').each(function(){
+        $('#myNavbar li a').each(function(){
             var $this = $(this);
             // if the current path is like this link, make it active
             if($this.attr('href').indexOf(current) !== -1){
