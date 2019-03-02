8616ed703d7ead9c04ea769d1ccc5f5d22dd0608
Arthur Funnell <arthur.funnell@student.manchester.ac.uk>
2019-03-02 14:47:10 +0000

    Adding active

--- Header - Footer/header.php
+++ Header - Footer/header.php
@@ -39,7 +39,7 @@
         </div>
         <div class="collapse navbar-collapse" id="myNavbar">
           <ul class="nav navbar-nav navbar-center" id="nav">
-            <li><a href="../Feed Page/Feed.php" class=""><span class="glyphicon glyphicon-list"></span> FEED</a></li>
+            <li><a href="web.cs.manchester.ac.uk/w81454af/Z3_Y1_Project/Feed%20Page/Feed.php" class=""><span class="glyphicon glyphicon-list"></span> FEED</a></li>
             <li><a href="../Notes Upload Page/NoteUpload.php" class=""><span class="glyphicon glyphicon-upload"></span> UPLOAD</a></li>
           </ul>
           <form method="post" class="navbar-form navbar-left form-style" action="../Search/search.php">
