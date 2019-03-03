11e1e7f5d697bfe772c2deb72235bedfa6e6c56b
c20808ar <abdulrahman.rafea@student.manchester.ac.uk>
2019-03-03 13:47:16 +0000

    working on user homepage

--- User Home Page/UserHomePage.php
+++ User Home Page/UserHomePage.php
@@ -1,31 +1,29 @@
 <?php
 session_start();
 ?>
-<!DOCTYPE html>
-<html>
-<head>
-<title>Page Title</title>
 <?php
 require_once("../Header - Footer/header.php");
 ?>
-</head>
+<title>Page Title</title>
+<style>
+body {
+	padding-top: 50px;
+}
+</style>
+
 <body>
+	<div id="top">
 <?php
 $_SESSION["username"] = $_SESSION["username"]
 ?>
-</br>
-</br>
-</br>
-</br>
-</br>
-</br>
+<h2>Hello THE TEAM <?php echo $_SESSION["username"]; ?></h2>
 <?php echo $_SESSION["username"]."</br>"; ?>
 <?php
 	require_once('config.inc.php');
     	// Connect to the database
      	$conn = new PDO("mysql:host=$database_host;dbname=$database_name", $database_user, $database_pass);
      	$conn2 = new mysqli($database_host, $database_user, $database_pass, "2018_comp10120_z3");
-	$UnitYear = $conn2->query("SELECT YearOfStudent FROM Users WHERE Username ='$_SESSION[username]'")->fetch_object()->YearOfStudent;//YearOfStudent query	
+	$UnitYear = $conn2->query("SELECT YearOfStudent FROM Users WHERE Username ='$_SESSION[username]'")->fetch_object()->YearOfStudent;//YearOfStudent query
 	$stat = $conn->prepare("SELECT * FROM Notes WHERE UnitYear = ?");
    	$stat->bindParam(1, $UnitYear);
    	$stat->execute();
@@ -35,15 +33,12 @@ $_SESSION["username"] = $_SESSION["username"]
       {
       echo "<li><a href='ShowNotes.php?id=".$row['UnitID']."&UnitYear=".$row['UnitYear']."'>".$row['UnitID']."</a></li>";
       array_push($UnitIDInField, $row['UnitID']);
-      }    
+      }
 }
-	
-
 ?>
+</div>
 </body>
-<footer>
 <?php
 require_once("../Header - Footer/footer.html");
 ?>
-</footer>
 </html>
