2c18505ce739c6d4ec9db9036eedbb25770cf6dc
Abdulrahman <abdulrahman.rafea@student.manchester.ac.uk>
2019-03-05 15:29:32 +0000

    salim missed things up2

--- Notes Page/NotesPreview.php
+++ Notes Page/NotesPreview.php
@@ -15,7 +15,7 @@ body {
 require_once('config.inc.php');
 $conn = new PDO("mysql:host=$database_host;dbname=$database_name", $database_user, $database_pass);
 $id = isset($_GET['id'])? $_GET['id'] : "";
-$stat = $conn->prepare("SELECT * FROM Notes WHERE UnitID = ? AND UnitYear = ?");
+$stat = $conn->prepare("SELECT * FROM Notes WHERE UnitID = ?");
 $stat->bindParam(1, $id);
 $stat->execute();
 $row = $stat->fetch();
