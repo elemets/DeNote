c9f8cfbb603ed78ba16871ad48d46d56ff98c72b
Abdulrahman <abdulrahman.rafea@student.manchester.ac.uk>
2019-03-05 15:27:15 +0000

    salim missed things up

--- Notes Page/NotesPreview.php
+++ Notes Page/NotesPreview.php
@@ -11,7 +11,19 @@ body {
   <div id="top" class="container-fluid">
   <div class="row">
     		<div class="col-sm-12">
-  	      <h4>Title</h4>
+<?php
+require_once('config.inc.php');
+$conn = new PDO("mysql:host=$database_host;dbname=$database_name", $database_user, $database_pass);
+$id = isset($_GET['id'])? $_GET['id'] : "";
+$stat = $conn->prepare("SELECT * FROM Notes WHERE UnitID = ? AND UnitYear = ?");
+$stat->bindParam(1, $id);
+$stat->execute();
+$row = $stat->fetch()
+echo '<h4>'
+echo $row['TitleNote']
+echo '</h4>'
+?>
+
           <h5> Author</h5>
           <h5> Time</h5>
         </div>
