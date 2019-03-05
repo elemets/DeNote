fbea1e96e36f7bd821417a3ac0f886395720b632
Martin <martin.cuestaallende@student.manchester.ac.uk>
2019-03-05 21:26:46 +0000

    Minor adjustments

--- User Home Page/UserHomePage.php
+++ User Home Page/UserHomePage.php
@@ -60,7 +60,7 @@ body > p {
 ?>
 </div>
 </div>
-<p><a style="font-size:10px;color:grey;text-align:center;" href="attributions.php">Image attributions</a></p>
+<p><a style="color:grey;" href="attributions.php">Image attributions</a></p>
 </body>
 <?php
 require_once("../Header - Footer/footer.html");
