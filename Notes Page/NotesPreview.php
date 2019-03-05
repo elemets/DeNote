3cc367e969359df4ec9426ac5a274e3ba059e2ae
Abdulrahman <abdulrahman.rafea@student.manchester.ac.uk>
2019-03-05 16:16:05 +0000

    salim missed things up3

--- Notes Page/NotesPreview.php
+++ Notes Page/NotesPreview.php
@@ -36,9 +36,13 @@ echo "Date Of Publish: " . $row['DateOfPublish'];
 echo '</h5>';
 ?>
         </div>
-  </div>
 <?php
 $id = "view.php?id=" . $_GET['id'];
+
+
+
+if ($row['dataType'] == "application/pdf")
+{
 ?>
     		<div class="col-sm-12">
 <div class="embed-responsive embed-responsive-4by3"
@@ -50,7 +54,20 @@ $id = "view.php?id=" . $_GET['id'];
 </div>
 </div>
 </div>
-      <div class="row justify-content-md-center">
+<?php
+}else {
+?>
+    		<div class="col-sm-9">
+		<img src="<?php echo $id ?>" style="width: 100%;">
+	</div>
+
+
+
+
+<?php
+}
+?>
+      <div class="justify-content-md-center">
        <div class="col-sm-9" style="margin:0% 12.5%;"
 <!-- begin wwww.htmlcommentbox.com -->
  <div id="HCB_comment_box" style="width:80%;margin-left:10%"><a href="http://www.htmlcommentbox.com">Widget</a> is loading comments...</div>
@@ -58,6 +75,7 @@ $id = "view.php?id=" . $_GET['id'];
  <script type="text/javascript" id="hcb"> /*<!--*/ if(!window.hcb_user){hcb_user={};} (function(){var s=document.createElement("script"), l=hcb_user.PAGE || (""+window.location).replace(/'/g,"%27"), h="//www.htmlcommentbox.com";s.setAttribute("type","text/javascript");s.setAttribute("src", h+"/jread?page="+encodeURIComponent(l).replace("+","%2B")+"&opts=16862&num=10&ts=1550317288312");if (typeof s!="undefined") document.getElementsByTagName("head")[0].appendChild(s);})(); /*-->*/ </script>
 <!-- end www.htmlcommentbox.com -->
        </div>
+</div>
 </div>
 </div>
 </body>
