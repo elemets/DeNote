b5e6ee3ff65be2901f7219f920708e7dac177f19
c20808ar <abdulrahman.rafea@student.manchester.ac.uk>
2019-03-05 23:52:55 +0000

    center text in shownotes

--- User Home Page/ShowNotes.php
+++ User Home Page/ShowNotes.php
@@ -43,7 +43,7 @@ $stat->execute();
 	<div class="col-sm-3">
 			    <div class="container">
 					< img src="squareElement.png" style="width:100%"
-<?php echo"href='../Notes Page/NotesPreview.php?id=".$row['NoteID']."'>"; ?>
+<?php echo "href='../Notes Page/NotesPreview.php?id=".$row['NoteID']."'>"; ?>
 			        <div class="centered"><?php echo $row['FileName'] ?></div>
 			        </div>
 	 </div>
