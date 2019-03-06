652ea15c60052013b3cd8f5c38c252f11343bdd9
c20808ar <abdulrahman.rafea@student.manchester.ac.uk>
2019-03-06 00:08:36 +0000

    center text in shownotes

--- User Home Page/ShowNotes.php
+++ User Home Page/ShowNotes.php
@@ -20,7 +20,7 @@ body > p {
 <body>
 	<div id="top" class="container-fluid">
 		<div class="row">
-		<div class="col-sm-3"
+		<div class="col-sm-12"
 		<h1 style="padding-bottom:30px;"><?php echo $_GET['id']; ?></h1>
 	</div>
 </div>
