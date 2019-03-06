26b37b6f00b29c1246283ea2aaebd8b347211058
[salim alwahaibi] <[salim.al-wahaibi@student.manchetser.ac.uk]>
2019-03-06 20:41:23 +0000

    voting system v39

--- User Home Page/ShowNotes.php
+++ User Home Page/ShowNotes.php
@@ -39,12 +39,16 @@ $stat->execute();
 
     while($row = $stat->fetch()){
 		$noteID = $row['NoteID'];
-		$stat2 = $conn->prepare("SELECT * FROM `Votes` WHERE NoteID = '$noteID' AND type = 1" );
+		$stat2 = $conn->prepare("SELECT * FROM `Votes` WHERE NoteID = '$noteID'");
 		$stat2->execute();
 		$counterLikes = 0;
+		$counterDislikes = 0;
 		while($row2 = $stat2->fetch())
 		{
-  		    $counterLikes++;
+			if($row2['type'] == 1)
+  				$counterLikes++;
+			else
+				$counterDislikes++;
 		}
 
 ?>	
@@ -53,7 +57,10 @@ $stat->execute();
 	<div class="col-sm-3">
 		      <a <?php echo "href='../Notes Page/NotesPreview.php?id=".$row['NoteID']."'>"; ?>
 					<img src="squareElement.png" style="width:100%">
-			        <div class="centered"><h2 style="color: #fff;"><?php echo $row['TitleNote'];?> </br> Likes: <?php echo $counterLikes; ?></h2></div>
+			        <div class="centered"><h2 style="color: #fff;">
+					<?php echo $row['TitleNote'];?> 
+					</br> Likes: <?php echo $counterLikes; ?> 
+					</br> Dislikes: <?php echo $counterDislikes; ?></h2></div>
 				  </a>
 	 </div>
 <?php
