ff13dfe64706a26d062c7b2a6b275258c439b436
[salim alwahaibi] <[salim.al-wahaibi@student.manchetser.ac.uk]>
2019-03-06 19:38:09 +0000

    voting system v26

--- Notes Page/NotesPreview.php
+++ Notes Page/NotesPreview.php
@@ -127,7 +127,7 @@ $stat = $conn->prepare("SELECT * FROM `Votes` WHERE NoteID = '$notes' AND UserID
 	
 if(($row = $stat->fetch()) != null)
 {
-  if("row['type']" == "1")
+  if("row['type']" == 1)
   {
 ?>
   <input type="submit" class="btn btn" method="post" value="Liked" name="btn2">
@@ -147,6 +147,15 @@ else
 <?php
 } ?>
 </form>
+
+
+
+
+
+
+
+
+
 
    <?php
 	$notes =  $_GET['id'];
@@ -169,7 +178,7 @@ $stat = $conn->prepare("SELECT * FROM `Votes` WHERE NoteID = '$notes' AND UserID
 	
 if(($row = $stat->fetch()) != null)
 {
-  if("row['type']" == "-1")
+  if("row['type']" == -1)
   {
   ?>
   <input type="submit" class="btn btn" method="post" value="disLiked" name="btn3">
