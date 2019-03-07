011309e129db91199a6b96d46e4f98c0b8ac8786
Arthur Funnell <arthur.funnell@manchester.ac.uk>
2019-03-07 18:12:52 +0000

    Styling comment box

--- Profile Page/Profile.php
+++ Profile Page/Profile.php
@@ -36,7 +36,7 @@ padding: 0px 0px;
 	background-color:#faf2ff;
 }
 
-input#HCB_comment_box .submit {
+input#HCB_submit .submit {
 	background-color: #faf2ff ;
 	border:1px solid #faf2ff;
 	color: #faf2ff;
