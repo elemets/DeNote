2fbd2c96a2cbb4d9f1564953f996dc6318f1f5a5
Abdulrahman <abdulrahman.rafea@student.manchester.ac.uk>
2019-03-07 17:55:46 +0000

    Download alignment

--- Notes Page/NotesPreview.php
+++ Notes Page/NotesPreview.php
@@ -38,6 +38,12 @@ body {
 .submit-btn {
 		background-color: #660099 !important;
 }
+like-btn {
+		background-color: green !important;
+}
+dislike-btn {
+		background-color: red !important;
+}
 .submit-font {
 		color:#ffffff !important;
 }
@@ -149,20 +155,20 @@ if(($row = $stat->fetch()) != null)
   if($typeVote == 1)
   {
 ?>
-  <button type="submit" class="btn btn-block submit-btn submit-font" method="post" value="Liked" name="btn2"><?php echo $counterLikes ?> <span class="glyphicon glyphicon-thumbs-up"> Liked</span></button>
+  <button type="submit" class="btn btn-block like-btn submit-font" method="post" value="Liked" name="btn2"><?php echo $counterLikes ?> <span class="glyphicon glyphicon-thumbs-up"> Liked</span></button>
 <?php
   }
   else
   {
 ?>
-<button type="submit" class="btn btn-block submit-btn submit-font" value="Like!" disabled><?php echo $counterLikes ?> <span class="glyphicon glyphicon-thumbs-up"> Like!</span></button>
+<button type="submit" class="btn btn-block like-btn submit-font" value="Like!" disabled><?php echo $counterLikes ?> <span class="glyphicon glyphicon-thumbs-up"> Like!</span></button>
 <?php
   }
 }
 else
 {
 ?>
-  <button type="submit" class="btn btn-block submit-btn submit-font" method="post" value="Like!" name="btn"><?php echo $counterLikes ?> <span class="glyphicon glyphicon-thumbs-up"> Like!</span></button>
+  <button type="submit" class="btn btn-block like-btn submit-font" method="post" value="Like!" name="btn"><?php echo $counterLikes ?> <span class="glyphicon glyphicon-thumbs-up"> Like!</span></button>
 <?php
 } ?>
 </form>
@@ -206,20 +212,20 @@ if(($row = $stat->fetch()) != null)
   if($typeVote == -1)
   {
   ?>
-	<button type="submit" class="btn btn-block submit-btn submit-font" method="post" value="disLiked" name="btn3"><?php echo $counterLikes ?> <span class="glyphicon glyphicon-thumbs-down"> Disliked</span></button>
+	<button type="submit" class="btn btn-block dislike-btn submit-font" method="post" value="disLiked" name="btn3"><?php echo $counterLikes ?> <span class="glyphicon glyphicon-thumbs-down"> Disliked</span></button>
 <?php
   }
   else
   {
   ?>
-  <button type="submit" class="btn btn-block submit-btn submit-font" value="disLike" disabled><?php echo $counterLikes ?> <span class="glyphicon glyphicon-thumbs-down"> Dislike</span></button>
+  <button type="submit" class="btn btn-block dislike-btn submit-font" value="disLike" disabled><?php echo $counterLikes ?> <span class="glyphicon glyphicon-thumbs-down"> Dislike</span></button>
 <?php
   }
 }
 else
 {
 ?>
-  <button type="submit" class="btn btn-block submit-btn submit-font" method="post" value="disLike" name="btn4"><?php echo $counterLikes ?> <span class="glyphicon glyphicon-thumbs-down"> DisLike</span></button>
+  <button type="submit" class="btn btn-block dislike-btn submit-font" method="post" value="disLike" name="btn4"><?php echo $counterLikes ?> <span class="glyphicon glyphicon-thumbs-down"> DisLike</span></button>
 <?php } ?>
 </form>
 </div>
