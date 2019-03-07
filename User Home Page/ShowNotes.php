e1d712e0993883fd43f6c51b77e4861dbeed2156
Bozhidar Nedelchev <bozhidar.nedelchev@student.manchester.ac.uk>
2019-03-07 17:59:00 +0000

    a little changes of everything

--- User Home Page/ShowNotes.php
+++ User Home Page/ShowNotes.php
@@ -32,7 +32,6 @@ body > p {
 </div>
 			<div class="row">
 <?php
-session_start();
 require_once('config.inc.php');
 $conn = new PDO("mysql:host=$database_host;dbname=$database_name", $database_user, $database_pass);
 $id = isset($_GET['id'])? $_GET['id'] : "";
