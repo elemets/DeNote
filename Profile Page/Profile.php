fe89f9a631e08c9094821a66761f2f66a1506a0d
c20808ar <abdulrahman.rafea@student.manchester.ac.uk>
2019-03-08 23:22:20 +0000

    IOS optimization

--- Profile Page/Profile.php
+++ Profile Page/Profile.php
@@ -82,15 +82,13 @@ body > p {
 
 <!-- Title and profile icon -->
 <div class="text-center">
-  <h1 style="font-size:60px;padding-top: 55px;"><?php echo $_SESSION['username']; ?></h1>
-  <?php
-  require_once('config.inc.php');
-  $conn = new PDO("mysql:host=$database_host;dbname=$database_name", $database_user, $database_pass);
-  $conn2 = new mysqli($database_host, $database_user, $database_pass, "2018_comp10120_z3");
-   ?>
-  <img src="Icons/Profile_Icon.png" alt="" style="width: 250px; height: auto;">
+	<div class="container">
+	<img src="squareElement.png" style="width:100%" class="img-circle">
+				<div class="centered"><h2 style="color: #fff;">
+	<?php echo $_SESSION['username']; ?>
+	</div>
+	</div>
 </div>
-
 
 <!-- My notes Section -->
 <div class="row">
@@ -213,12 +211,9 @@ $userID = $conn2->query("SELECT UserID FROM Users WHERE Username ='$_SESSION[use
 <?php
 }
 ?>
-
   </div>
-</div>
 <!-- Followers Section End -->
 </div>
-
 </body>
 <?php
 require_once("../Header - Footer/footer.html");
