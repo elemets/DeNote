286c3f507e442589f2e5243a54c8eb8a556b1528
c20808ar <abdulrahman.rafea@student.manchester.ac.uk>
2019-03-08 20:01:04 +0000

    IOS optimization

--- Notes Page/NotesPreview.php
+++ Notes Page/NotesPreview.php
@@ -135,7 +135,7 @@ if ($row['dataType'] == "application/pdf")
 }
 ?>
 
-<div class="row" style="text-align:center; padding-bottom:20px;">
+<div class="row" style="text-align:center;">
 	<div class="col-sm-2"></div>
 <!-- VOTING -->
 
