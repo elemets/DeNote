fc2104e9b579ee032b03ca0c34c45a531e759750
c20808ar <abdulrahman.rafea@student.manchester.ac.uk>
2019-03-08 23:38:54 +0000

    IOS optimization

--- Profile Page/Profile.php
+++ Profile Page/Profile.php
@@ -87,7 +87,7 @@ body > p {
    ?>
 
 <div class="row">
-<div class="col-sm-4 col-sm-offset-4">
+<div class="col-sm-2 col-sm-offset-5">
 <a href= "<?php echo $links[$counter]; ?>">
 <img src="squareElement.png" style="width:100%" class="img-circle">
 			<div class="centered"><h1 style="color: #fff; font-size: 50px;">
