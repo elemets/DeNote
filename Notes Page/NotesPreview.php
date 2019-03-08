6a40dfa3ef634e3bbd0af33d18bca899fadf7b08
c20808ar <abdulrahman.rafea@student.manchester.ac.uk>
2019-03-08 20:43:29 +0000

    IOS optimization

--- Notes Page/NotesPreview.php
+++ Notes Page/NotesPreview.php
@@ -128,6 +128,7 @@ if ($row['dataType'] == "application/pdf")
 
 <div class="col-sm-2 mobile-pdf"></div>
 <div class="col-sm-8 mobile-pdf">
+<a href="<?php echo $id ?>" class="btn btn-block submit-btn submit-font" role="button"><span class="glyphicon glyphicon-download-alt"></span> View PDF</a>
 <a href="<?php echo $id ?>" class="btn btn-block submit-btn submit-font" role="button">View PDF</a>
 </div>
 <div class="col-sm-2 mobile-pdf"></div>
