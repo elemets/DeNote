53a06fa068be69239c20592ec6750d5328405bf2
Bozhidar Nedelchev <bozhidar.nedelchev@student.manchester.ac.uk>
2019-03-09 12:13:54 +0000

    edit page

--- Profile Page/EditPage.php
+++ Profile Page/EditPage.php
@@ -5,7 +5,15 @@
     <title>Edit Profile</title>
   </head>
   <body>
-    <form method="post">
+    <form method="post" role="form" method="post" action="<?php echo $_SERVER['PHP_SELF'];?>">
+
+      <!--USERNAME-->
+      <label for="username">Username (mustn't contain <>)</label>
+      <input type="textbox" class="form-control form-element" name="username" placeholder="Username" pattern="[^<>]+">
+
+      <!--PASSWORD-->
+      <label for="password">Password (8+ characters, mustn't contain <>)</label>
+      <input type="password" class="form-control form-element" name="password" placeholder="Password" pattern="[a-zA-Z0-9!?@#$%*-/+_]{8,}">
 
     </form>
   </body>
