aa7088cf823ba3ade3edc1f4f7590363eee2ae2f
[salim alwahaibi] <[salim.al-wahaibi@student.manchetser.ac.uk]>
2019-03-09 12:41:33 +0000

    adding pic in notes

--- Search/search.php
+++ Search/search.php
@@ -17,12 +17,12 @@ if($_SESSION["username"] == null)
     $conn = new PDO("mysql:host=$database_host;dbname=$database_name", $database_user, $database_pass);
     // $conn = new mysqli($database_host, $database_user, $database_pass, "2018_comp10120_z3");
     $searchWord = $_POST['searchWord'];
-    $stat = $conn->prepare("SELECT * FROM Notes WHERE * LIKE '%?%'");
+    $stat = $conn->prepare("SELECT * FROM Notes WHERE TitleNote LIKE '%?%'");
     $stat->bindParam(1, $searchWord);
     $stat->execute();
     while($row = $stat->fetch())
     {
-      echo $row['FileName'];
+      echo $row['TitleNote'];
       if ($row['FileName'] == $searchWord || $row['TitleNote'] == $searchWord || $row['UnitID'] == $searchWord)
       {
               echo "<li><a  href='../Notes Page/NotesPreview.php?id=".$row['NoteID']."'>".$row['FileName']."</a></li>";
