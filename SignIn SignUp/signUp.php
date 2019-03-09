ee6a4844e6d775b85a8901d66a144fc9f9118406
Abdullah <abdullah.ali-5@student.manchester.ac.uk>
2019-03-09 16:27:32 +0000

    changed to signup form

--- SignIn SignUp/signUp.php
+++ SignIn SignUp/signUp.php
@@ -151,8 +151,8 @@
        header('Location: ../User Home Page/UserHomePage.php');
   	  } else {
         $action['result'] = 'error';
-        array_push($text,'User could not be added to the database. Reason: ' . mysql_error());
-        =
+        array_push($text,'User could not be added to the database. Reason: ' .mysql_error());
+
         }
 
 
