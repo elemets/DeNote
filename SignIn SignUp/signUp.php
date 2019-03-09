a4de0301b510b39a77eb44076c0ba1c7e066c606
Abdullah <abdullah.ali-5@student.manchester.ac.uk>
2019-03-09 16:11:09 +0000

    changed to signup form

--- SignIn SignUp/signUp.php
+++ SignIn SignUp/signUp.php
@@ -148,9 +148,9 @@
         $action['result'] = 'error';
         array_push($text,'User could not be added to the database. Reason: ' . mysql_error());
         }
-         ?>
 
-    <!--
+
+
     <div class="fixed-top" style="padding-top: 53px">
     	<div class="alert alert-danger alert-dismissible fade show" role="alert">
         <strong>Error:</strong> Please fill in all the fields correctly.
@@ -159,7 +159,6 @@
         </button>
       </div>
     </div>
-  -->
-  }
+    }  ?>
 </body>
 </html>
