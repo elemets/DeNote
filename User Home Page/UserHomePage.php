4cad861f9f8db070b268c43f712116972c330f1c
Bozhidar Nedelchev <bozhidar.nedelchev@student.manchester.ac.uk>
2019-03-09 18:46:38 +0000

    bug on sign up  page

--- User Home Page/UserHomePage.php
+++ User Home Page/UserHomePage.php
@@ -37,7 +37,8 @@ body > p {
     	// Connect to the database
      	$conn = new PDO("mysql:host=$database_host;dbname=$database_name", $database_user, $database_pass);
      	$conn2 = new mysqli($database_host, $database_user, $database_pass, "2018_comp10120_z3");
-	$UnitYear = $conn2->query("SELECT YearOfStudent FROM Users WHERE Username ='$_SESSION["username"]'")->fetch_object()->YearOfStudent;//YearOfStudent query
+			$username = $_SESSION["username"];
+	$UnitYear = $conn2->query("SELECT YearOfStudent FROM Users WHERE Username = '$username'")->fetch_object()->YearOfStudent;//YearOfStudent query
 	$stat = $conn->prepare("SELECT * FROM Notes WHERE UnitYear = ?");
    	$stat->bindParam(1, $UnitYear);
    	$stat->execute();
