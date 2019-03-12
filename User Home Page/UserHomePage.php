59065966a6c9679baaf69e1262cd2f7291fd9e53
Abdulrahman <abdulrahman.rafea@student.manchester.ac.uk>
2019-03-12 13:10:55 +0000

    profile

--- User Home Page/UserHomePage.php
+++ User Home Page/UserHomePage.php
@@ -50,7 +50,7 @@ body > p {
 ?>
 		<div class="col-sm-3 col-xs-6">
     <a <?php echo"<a href='ShowNotes.php?id=".$row['UnitID']."&UnitYear=".$row['UnitYear']."'>"; ?>
-			        <img src="squareElement.png" style="width:100%">
+			        <img src="<?php echo $img ?>" style="width:100%">
 			        <div class="centered"><h2 style="color: #fff;"><?php echo $row['UnitID'] ?></h2></div>
 				  </a>
 	 </div>
