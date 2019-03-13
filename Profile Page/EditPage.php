6fb3115b82c2bcf02950eb86a3e7e260643ab7af
c20808ar <abdulrahman.rafea@student.manchester.ac.uk>
2019-03-13 17:56:17 +0000

    Styling

--- Profile Page/EditPage.php
+++ Profile Page/EditPage.php
@@ -72,10 +72,13 @@ session_start();
 		<div class="form-body">
 	       <form class="form-horizontal" role="form" method="post" action="<?php echo $_SERVER['PHP_SELF'];?>">
 
+		<div class="form-body form-check">
+					 <input type="checkbox" class="form-control form-element form-check-input" name="box" id="box" value="withName">
+	 				<label for="box" class="form-check-label"> I want to change username too</label>
+    </div>
+
 	  <div class="form-group">
         <label for="username"> New Username (mustn't contain <>)</label>
-				<input type="checkbox" class="form-control form-element form-check-input" name="box" id="box" value="withName">
-				<label for="box" class="form-check-label"> I want to change username too</label>
         <input type="textbox" class="form-control form-element" name="newUsername" id="text" placeholder="Username" pattern="[^<>]+" value="<?php echo $_SESSION['username'];?>" disabled>
     </div>
 
