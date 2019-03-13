259ae11536f6b5773f4c92b569996d4ce16109d3
c20808ar <abdulrahman.rafea@student.manchester.ac.uk>
2019-03-13 13:32:24 +0000

    styling

--- Profile Page/Profile.php
+++ Profile Page/Profile.php
@@ -206,7 +206,7 @@ body > p {
 				<div class="col-sm-6">
 						<h3 style="font-size: 30px; padding-top: 30px; padding-bottom: 15px; color: black;">My notes</h3>
 					</div>
-					<div class="col-sm-6 text-right" style="padding-top: 30px; padding-bottom: 15px; color: black;">
+					<div class="col-sm-6" style="padding-top: 30px; padding-bottom: 15px; color: black;">
 						<div class="container">
 						<h4 style="color: black;">Delete Note:</h4>
 						<form method="post" class"form-inline">
