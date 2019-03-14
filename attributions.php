9198cea92be0a16568e4d808923cc4f077bdfa83
Martin <martin.cuestaallende@student.manchester.ac.uk>
2019-03-14 13:43:06 +0000

    Trying to fix some problems displaying the page correctly

--- attributions.php
+++ attributions.php
@@ -3,13 +3,7 @@
 <head>
     <title>Attributions</title>
     <meta charset="utf-8">
-    <meta name="viewport" content="width=device-width, initial-scale=1">
-    <link rel="stylesheet" href="https://maxcdn.bootstrapcdn.com/bootstrap/3.4.0/css/bootstrap.min.css">
-    <link rel="stylesheet" href="Welcome Page/welcomeStyleSheet.css">
-    <link href="https://fonts.googleapis.com/css?family=Montserrat" rel="stylesheet" type="text/css">
-    <link href="https://fonts.googleapis.com/css?family=Lato" rel="stylesheet" type="text/css">
-    <script src="https://ajax.googleapis.com/ajax/libs/jquery/3.3.1/jquery.min.js"></script>
-    <script src="https://maxcdn.bootstrapcdn.com/bootstrap/3.4.0/js/bootstrap.min.js"></script>
+    <link rel="stylesheet" href="universalStyleSheet.css">
 </head>
 <?php
 require_once("Header - Footer/header.php");
