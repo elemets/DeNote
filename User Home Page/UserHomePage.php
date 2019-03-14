f128ceaf5e788d0b6ec66a0b26d7b45b62a83ad7
Martin <martin.cuestaallende@student.manchester.ac.uk>
2019-03-14 15:10:09 +0000

    Trying to see if presentation improves

--- User Home Page/UserHomePage.php
+++ User Home Page/UserHomePage.php
@@ -21,7 +21,7 @@
       top: 50%;
       left: 50%;
       background-color:rgba(0, 0, 0, 0.8);
-      padding: 10% 10% 10% 10%;
+      padding: 15px 10% 15px 10%;
       transform: translate(-50%, -75%);
       text-align: center;
     }
