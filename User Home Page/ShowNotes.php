f6bb5a0207efcdcae26219880fa1feb6b3bcab2a
Martin <martin.cuestaallende@student.manchester.ac.uk>
2019-03-14 15:19:29 +0000

    Improving presentation

--- User Home Page/ShowNotes.php
+++ User Home Page/ShowNotes.php
@@ -11,20 +11,24 @@
   <title>Page Title</title>
   <style>
     body {
-    padding-top: 50px;
+     padding-top: 50px;
     }
     body > p {
-    text-align: center;
+     text-align: center;
     }
     .centered {
-    position: absolute;
-    top: 50%;
-    left: 50%;
-    transform: translate(-50%, -100%);
-    text-align: center;
+      position: absolute;
+      top: 50%;
+      left: 50%;
+      outline-style: solid;
+      outline-color: #000;
+      background-color:rgba(255, 255, 255, 0.9);
+      padding: 15px 10% 15px 10%;
+      transform: translate(-50%, -75%);
+      text-align: center;
     }
     .col-sm-3 {
-    padding-bottom: 30px;
+      padding-bottom: 30px;
     }
   </style>
 
