361380f6a6c79bfb497c873bde1c33d1549496d0
Bozhidar Nedelchev <bozhidar.nedelchev@student.manchester.ac.uk>
2019-03-15 12:07:19 +0000

    Adding the test upload page

---
+++ Notes Upload Page/NoteUploadTest.php
@@ -0,0 +1,64 @@
+<?php
+session_start();
+?>
+<!DOCTYPE html>
+<html>
+<head>
+<title>Page Title</title>
+<?php
+//require_once("../Header - Footer/header.html");
+?>
+</head>
+<body>
+    <?php
+    require_once('config.inc.php');
+    // Connect to the database
+     $conn = new PDO("mysql:host=$database_host;dbname=$database_name", $database_user, $database_pass);
+     $conn2 = new mysqli($database_host, $database_user, $database_pass, "2018_comp10120_z3");
+
+     $conn->setAttribute(PDO::ATTR_ERRMODE, PDO::ERRMODE_EXCEPTION);
+
+    if(isset($_POST['btn'])){
+      $userID = $conn2->query("SELECT UserID FROM Users WHERE Username ='$_SESSION[username]'")->fetch_object()->UserID;
+      $name = $_FILES['requiredFile']['name'];
+      $type = $_FILES['requiredFile']['type'];
+      $data = file_get_contents($_FILES['requiredFile']['tmp_name']);
+      $sectionName = $_POST["sectionName"];
+      $parentID = $_POST["courses"];
+      $stmt = $conn->prepare("INSERT INTO Notes (`FileName`,`dataType`,`Data`, `SectionName`, `UserID`, `ParentID`) VALUES (?, ?, ?,?, ?,?)");
+      $stmt->bindParam(1, $name);
+      $stmt->bindParam(2, $type);
+      $stmt->bindParam(3, $data);
+      $stmt->bindParam(4, $sectionName);
+      $stmt->bindParam(5, $userID);
+      $stmt->bindParam(6, $parentID);
+      $stmt->execute();
+    }
+     ?>
+    <form method="post" enctype="multipart/form-data">
+      <label for="sectionID"> Section:</label>
+      <input type="textbox" name="sectionName"/>
+      <label for="uploadedFile"> Choose file:</label>
+      <input type="file" name="requiredFile"/>
+      <label for="Unit"> Section:</label>
+      <select name="courses" placeholder="parentID">
+              <option>AHCP</option>
+              <option>AMER</option>
+              <option>ARGY</option>
+              <option>BIOL</option>
+              <option>BMAN</option>
+              <option>CHEM</option>
+              <option>Other</option>
+      </select>
+      <button name="btn"> Upload </button>
+    </form>
+
+    <?php
+    $stat = $conn->prepare("SELECT * FROM `Notes`");
+    $stat->execute();
+     ?>
+
+</body>
+<footer>
+</footer>
+</html>
