c41d7f902814134f2b6f0060f1552f2af57d93c8
[salim alwahaibi] <[salim.al-wahaibi@student.manchetser.ac.uk]>
2019-03-15 12:35:47 +0000

    saerch

--- Notes Upload Page/NoteUpload.php
+++ Notes Upload Page/NoteUpload.php
@@ -246,8 +246,9 @@
           else if ($Number == "")
             return false;
           else if($type != "image/jpeg" || $type != "application/pdf" || $type != "application/png")
+{
             echo $type;
-            return false;
+}
           else
             return true;
         }
