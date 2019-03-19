168eb2c97ff86685e24f355aa14dda32bb46dda3
Bozhidar Nedelchev <bozhidar.nedelchev@student.manchester.ac.uk>
2019-03-19 20:32:38 +0000

    merge photos

--- Notes Upload Page/test.php
+++ Notes Upload Page/test.php
@@ -20,7 +20,9 @@ if(isset($_POST['btn'])){
 
   $pdf->Output('example_009.pdf', 'F');
   $data = file_get_contents('example_009.pdf');
+  echo data;
   $type = filetype ('example_009.pdf');
+  echo $type;
   $stmt = $conn->prepare("INSERT INTO Notes (`FileName`,`dataType`,`Data`, `SectionNumber`, `UserID`, `UnitID`, `TitleNote`) VALUES (?,?,?,?,?,?,?)");
   $stmt->bindParam(1, 'example_009');
   $stmt->bindParam(2, $type);
