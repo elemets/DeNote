ced0a566af7c0ba5c51165bbbe5958b611883b78
Bozhidar Nedelchev <bozhidar.nedelchev@student.manchester.ac.uk>
2019-03-19 19:13:19 +0000

    merge photos

---
+++ Notes Upload Page/test.php
@@ -0,0 +1,20 @@
+<?php
+
+require_once('../Notes Upload Page/tcpdf/examples/tcpdf_include.php');
+require_once('../Notes Upload Page/tcpdf/tcpdf.php');
+session_start();
+require_once('config.inc.php');
+// Connect to the database
+ $conn = new PDO("mysql:host=$database_host;dbname=$database_name", $database_user, $database_pass);
+ $conn2 = new mysqli($database_host, $database_user, $database_pass, "2018_comp10120_z3");
+
+ $conn->setAttribute(PDO::ATTR_ERRMODE, PDO::ERRMODE_EXCEPTION);
+
+ $pdf = new TCPDF(PDF_PAGE_ORIENTATION, PDF_UNIT, PDF_PAGE_FORMAT, true, 'UTF-8', false);
+if(isset($_POST['btn'])){
+    $data = file_get_contents($_FILES['requiredFile']['tmp_name']);
+    $pdf->AddPage();
+    $pdf->Image('@'.$data);
+  }
+  $pdf->Output('example_009.pdf', 'I');
+ ?>
