1c470ee786da55491c7cbd72aa06f9c94c6078ea
[salim alwahaibi] <[salim.al-wahaibi@student.manchetser.ac.uk]>
2019-03-21 19:38:57 +0000

    asdas

--- Profile Page/Profile.php
+++ Profile Page/Profile.php
@@ -126,9 +126,18 @@ padding: 0px 10px;
 <script src="path/to/bootstrap.js"></script>
 <script src="path/to/bootstrap-confirmation.js"></script>
 <script>
-$('[data-toggle=confirmation]').confirmation({
-  rootSelector: '[data-toggle=confirmation]',
-  // other options
+$(document).ready(function() {
+    $('a[data-confirm]').click(function(ev) {
+        var href = $(this).attr('href');
+
+        if (!$('#dataConfirmModal').length) {
+            $('body').append('<div id="dataConfirmModal" class="modal" role="dialog" aria-labelledby="dataConfirmLabel" aria-hidden="true"><div class="modal-header"><button type="button" class="close" data-dismiss="modal" aria-hidden="true">×</button><h3 id="dataConfirmLabel">Please Confirm</h3></div><div class="modal-body"></div><div class="modal-footer"><button class="btn" data-dismiss="modal" aria-hidden="true">Cancel</button><a class="btn btn-primary" id="dataConfirmOK">OK</a></div></div>');
+        }
+        $('#dataConfirmModal').find('.modal-body').text($(this).attr('data-confirm'));
+        $('#dataConfirmOK').attr('href', href);
+        $('#dataConfirmModal').modal({show:true});
+        return false;
+    });
 });
 </script>
 <div class="row">
@@ -173,7 +182,7 @@ $('[data-toggle=confirmation]').confirmation({
 								}
 	?>
 					</select>
-							<input type="submit" class="btn btn-sm delete-btn submit-font bottom-buffer" data-toggle="confirmation" value="Delete" name="deleteBtn">
+							<input type="submit" class="btn btn-sm delete-btn submit-font bottom-buffer" data-confirm="Are you sure?" value="Delete" name="deleteBtn">
 						</form>
 					</div>
 				</div>
@@ -291,7 +300,7 @@ $('[data-toggle=confirmation]').confirmation({
 
 	?>
 							</select>
-							<input type="submit" class="btn btn-sm delete-btn submit-font bottom-buffer" data-toggle="confirmation" value="Delete" name="deleteBtn">
+							<input type="submit" class="btn btn-sm delete-btn submit-font bottom-buffer" data-confirm="Are you sure?" value="Delete" name="deleteBtn">
 						</form>
 					</div>
 				</div>
